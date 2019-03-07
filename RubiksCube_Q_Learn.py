07396e9c6d8b1ad4be181b72fc9a4fcafb56c466
Dustin Langner <dustin.langner1@gmail.com>
2019-03-06 16:54:56 -0800

    Initial attempt at Rubiks Cube MDP; some general clean up

---
+++ RubiksCube_Q_Learn.py
@@ -0,0 +1,7 @@
+'''RubiksCube_Q_Learn.py
+
+Brian Yu and Dustin Langner
+CSE 415, Winter 2019, Project
+March 11th, 2019
+
+'''
---
+++ RubiksCube_agent.py
@@ -0,0 +1,7 @@
+'''RubiksCube_agent.py
+
+Brian Yu and Dustin Langner
+CSE 415, Winter 2019, Project
+March 11th, 2019
+
+'''
